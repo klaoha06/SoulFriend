9b124668bfbd6ed8ab8ae94538c60734b46ef5d0
klaoha06 <laohasongkram.supakorn@gmail.com>
2015-03-26 21:00:40 +0700

    add $ add grunt rev task

--- Gruntfile.js
+++ Gruntfile.js
@@ -232,8 +232,8 @@ module.exports = function (grunt) {
       dist: {
         files: {
           src: [
-            '<%= yeoman.dist %>/public/{,*/}*.js',
-            '<%= yeoman.dist %>/public/{,*/}*.css',
+            '<%= yeoman.dist %>/public/{,*/}*.js$',
+            '<%= yeoman.dist %>/public/{,*/}*.css$',
             '<%= yeoman.dist %>/public/assets/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
             '<%= yeoman.dist %>/public/assets/fonts/*'
           ]
